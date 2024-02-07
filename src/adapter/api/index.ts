89d29e85f236c1f3559c012ef763a6026d65efc6
fs0414 <fujitanisora0414@gmail.com>
2024-02-07 14:33:45 +0900

    fix: comment delete

--- src/adapter/api/index.ts
+++ src/adapter/api/index.ts
@@ -1,12 +1,6 @@
-// import { app } from "../../index";
-// import express from "express";
 const express = require("express")
-
 const router = express.Router();
-// export const router = express.Router();
 
-// router.use("/api", () => {
-//     console.log("/api index.ts router")
-// });
 router.use("/api", require("./routes/board"));
+
 module.exports = router;
